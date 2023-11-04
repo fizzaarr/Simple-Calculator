7f820fc7b544e2fff67daeb1d0b79fecd8a53185
Fizza <fizzarasool749@gmail.com>
2023-11-04 13:50:29 +0500

    deleted index.js

--- app.js
+++ app.js
@@ -1,25 +1,51 @@
-let buttons = document.querySelector(".buttons");
-let btn = document.querySelectorAll("span");
-let screen = document.getElementById("screen");
-let toggleBtn = document.querySelector(".toggleBtn");
-let body = document.querySelector("body");
-
-for(let i=0; i<btn.length; i++){
-    btn[i].addEventListner("click",function(){
-        if(this.innerHTML=="="){
-            screen.innerHTML = eval(screen.innerHTML);
+document.addEventListener("DOMContentLoaded", function () {
+    let screen = document.getElementById("screen");
+    let buttons = document.querySelectorAll(".buttons span");
+    let clear = document.getElementById("clc");
+    let equal = document.getElementById("equal");
+    let darkMode = false;
+    let toggleBtn = document.querySelector(".toggleBtn");
+  
+    let equation = "";
+    let shouldClearScreen = false;
+  
+    buttons.forEach((button) => {
+      button.addEventListener("click", function () {
+        let buttonText = button.textContent;
+  
+        if (buttonText === "clear") {
+          equation = "";
+        } else if (buttonText === "=") {
+          try {
+            let result = eval(equation);
+            screen.textContent = result;
+            equation = result;
+          } catch (error) {
+            screen.textContent = "Error";
+          }
+        } else {
+          if (shouldClearScreen) {
+            screen.textContent = "";
+            shouldClearScreen = false;
+          }
+          equation += buttonText;
+          screen.textContent += buttonText;
         }
-        else{
-            if(this.innerHTML=="clear "){
-                screen.innerHTML="";
-            }
-            else{
-                screen.innerHTML += this.innerHTML;
-            }
-        }
-
+      });
     });
-}
-toggleBtn.onclick = function(){
-    body.classList.toggle("dark");
-}
+  
+    clear.addEventListener("click", function () {
+      screen.textContent = "";
+      equation = "";
+    });
+  
+    toggleBtn.addEventListener("click", function () {
+      darkMode = !darkMode;
+      if (darkMode) {
+        document.body.classList.add("dark");
+      } else {
+        document.body.classList.remove("dark");
+      }
+    });
+  });
+  
